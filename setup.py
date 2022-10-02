327de05c399bac38fde8651f7ecd3573e8bef92f
Brandon Martin-Anderson <badhill@gmail.com>
2022-10-01 18:00:10 -0700

    switch to an even better c module stub

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-cmodels = Extension('cmodels', sources=['cmodels.c'])
+cmodels = Extension('cmodels', sources=['cmodelsmodule.c'])
 
 setup(name="lazpy",
       version="0.1",
