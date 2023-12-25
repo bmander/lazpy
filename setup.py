b1c21a8aace9abba512b275f58ae7f838e703cf0
Brandon Martin-Anderson <badhill@gmail.com>
2023-12-24 18:41:02 -0800

    Update Extension name in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-cmodels = Extension('cpylaz', sources=['cpylazmodule.c'])
+cpylaz = Extension('cpylaz', sources=['cpylazmodule.c'])
 
 setup(name="lazpy",
       version="0.1",
